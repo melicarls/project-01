a01a2534ba368901ca3bb948e7104a52003e7867
melicarls <melissacarlson15@gmail.com>
2016-04-06 14:55:18 -0700

    NPM init and successful connection to Weather Underground API

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,27 @@
+var weatherEndpoint = "http://api.wunderground.com/api/4fefd5989e452ef5/forecast/q/CA/San_Francisco.json";
+
 $(document).ready(function() {
   console.log('app.js loaded!');
+
+  getWeather();
 
 });
 
 
   function getWeather() {$.ajax({
     method: "GET",
-    url: weatherendpoint,
+    url: weatherEndpoint,
     success: onSuccess,
     error: onError,
     });
   }
+
+function onSuccess(json) {
+  console.log("onSuccess was called because we got weather from WU");
+  console.log(json);
+  $('#weatherText').text(json.forecast.txt_forecast.forecastday[0].fcttext);
+}
+
+function onError() {
+  alert("Something went wrong. The weather could not be found");
+}
